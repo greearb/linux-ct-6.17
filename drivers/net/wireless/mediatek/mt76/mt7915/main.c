e9c3b4c84a4739e91f092a5074e1e01f478f097f
Howard Hsu <howard-yh.hsu@mediatek.com>
2024-05-30 16:08:54 +0800

    mtk: mac80211: add new argument link_id in set_bitrate_mask
    mt76 driver needs link id to fix parital rate on the specific link.
    
    Signed-off-by: Howard Hsu <howard-yh.hsu@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt7915/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7915/main.c
@@ -1396,7 +1396,8 @@ static void mt7915_sta_rc_update(struct ieee80211_hw *hw,
 
 static int
 mt7915_set_bitrate_mask(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
-			const struct cfg80211_bitrate_mask *mask)
+			const struct cfg80211_bitrate_mask *mask,
+			unsigned int link_id)
 {
 	struct mt7915_vif *mvif = (struct mt7915_vif *)vif->drv_priv;
 	struct mt7915_phy *phy = mt7915_hw_phy(hw);
