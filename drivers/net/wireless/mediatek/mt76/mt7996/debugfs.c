ee9369e981e7a124f75bf7178e077bc5113b6f2b
Ben Greear <greearb@candelatech.com>
2025-05-29 12:20:05 -0700

    mtk7996: debugs protect from null fwlog
    Should probably backport this, was from big debug patch.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/debugfs.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/debugfs.c
@@ -2182,6 +2182,9 @@ mt7996_debugfs_write_fwlog(struct mt7996_dev *dev, const void *hdr, int hdrlen,
 	unsigned long flags;
 	void *dest;
 
+	if (!dev->relay_fwlog)
+		return;
+
 	spin_lock_irqsave(&lock, flags);
 	dest = relay_reserve(dev->relay_fwlog, hdrlen + len + 4);
 	if (dest) {
