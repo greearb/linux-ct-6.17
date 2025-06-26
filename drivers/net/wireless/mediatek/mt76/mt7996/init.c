3e7cabf1fa7708fd9c93e69610129d318b7df8dc
Rory Little <rory@candelatech.com>
2025-06-26 11:28:43 -0700

    wifi: mt76: mt7996: Update global iface combinations with per-type info
    Some user-space programs naively derive the valid interface combinations
    per-radio by dividing these global limits by 3; they should be more
    specific to not break these use cases.
    
    Signed-off-by: Rory Little <rory@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/init.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/init.c
@@ -84,19 +84,23 @@ static const struct ieee80211_iface_combination if_comb = {
 	.beacon_int_min_gcd = 100,
 };
 
-static const struct ieee80211_iface_limit if_limits_repeater_global = {
-	.max = MT7996_MAX_INTERFACES_REPEATER * MT7996_MAX_RADIOS,
-	.types = BIT(NL80211_IFTYPE_STATION)
-		 | BIT(NL80211_IFTYPE_ADHOC)
-		 | BIT(NL80211_IFTYPE_AP)
+static const struct ieee80211_iface_limit if_limits_repeater_global[] = {
+	{
+		.max = 16 * MT7996_MAX_RADIOS,
+		.types = BIT(NL80211_IFTYPE_AP)
+			 | BIT(NL80211_IFTYPE_ADHOC)
 #ifdef CONFIG_MAC80211_MESH
-		 | BIT(NL80211_IFTYPE_MESH_POINT)
+			 | BIT(NL80211_IFTYPE_MESH_POINT)
 #endif
+	}, {
+		.max = MT7996_MAX_REPEATER_STA * MT7996_MAX_RADIOS,
+		.types = BIT(NL80211_IFTYPE_STATION)
+	}
 };
 
 static const struct ieee80211_iface_combination if_comb_repeater_global = {
-	.limits = &if_limits_repeater_global,
-	.n_limits = 1,
+	.limits = if_limits_repeater_global,
+	.n_limits = ARRAY_SIZE(if_limits_repeater_global),
 	.max_interfaces = MT7996_MAX_INTERFACES_REPEATER * MT7996_MAX_RADIOS,
 	.num_different_channels = MT7996_MAX_RADIOS,
 	.radar_detect_widths = BIT(NL80211_CHAN_WIDTH_20_NOHT) |
