greearb/linux-ct-6.11

wifi: iwlwifi: Fix eMLSR band comparison.
Do not make assumptions about what band 'a' or 'b' are on.
And add new reason code for when eMLSR is disabled due to
band.

Signed-off-by: Ben Greear <greearb@candelatech.com>

drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -368,6 +368,7 @@ struct iwl_mvm_vif_link_info {
  *	preventing the enablement of EMLSR
  * @IWL_MVM_ESR_EXIT_CSA: CSA happened, so exit EMLSR
  * @IWL_MVM_ESR_EXIT_LINK_USAGE: Exit EMLSR due to low tpt on secondary link
+ * @IWL_MVM_ESR_EXIT_BAND: Exit EMLSR due to incompatible Bands
  */
 enum iwl_mvm_esr_state {
 	IWL_MVM_ESR_BLOCKED_PREVENTION	= 0x1,
@@ -382,6 +383,7 @@ enum iwl_mvm_esr_state {
 	IWL_MVM_ESR_EXIT_BANDWIDTH	= 0x80000,
 	IWL_MVM_ESR_EXIT_CSA		= 0x100000,
 	IWL_MVM_ESR_EXIT_LINK_USAGE	= 0x200000,
+	IWL_MVM_ESR_EXIT_BAND		= 0x400000,
 };
 
 #define IWL_MVM_BLOCK_ESR_REASONS 0xffff

greearb/linux-ct-6.17

drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -375,6 +375,7 @@ struct iwl_mvm_vif_link_info {
  * @IWL_MVM_ESR_EXIT_CSA: CSA happened, so exit EMLSR
  * @IWL_MVM_ESR_EXIT_LINK_USAGE: Exit EMLSR due to low tpt on secondary link
  * @IWL_MVM_ESR_EXIT_FAIL_ENTRY: Exit EMLSR due to entry failure
+ * @IWL_MVM_ESR_EXIT_BAND: Exit EMLSR due to incompatible Bands
  */
 enum iwl_mvm_esr_state {
 	IWL_MVM_ESR_BLOCKED_PREVENTION	= 0x1,
@@ -391,6 +392,7 @@ enum iwl_mvm_esr_state {
 	IWL_MVM_ESR_EXIT_CSA		= 0x100000,
 	IWL_MVM_ESR_EXIT_LINK_USAGE	= 0x200000,
 	IWL_MVM_ESR_EXIT_FAIL_ENTRY	= 0x400000,
+	IWL_MVM_ESR_EXIT_BAND		= 0x800000,
 };
 
 #define IWL_MVM_BLOCK_ESR_REASONS 0xffff