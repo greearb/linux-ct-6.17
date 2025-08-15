22da9515d23d69a617a2058c97d495d49e978774
Ben Greear <greearb@candelatech.com>
2025-08-15 16:24:11 -0700

    mt7996:  Fix MLO packet reordering.
    From Evelyn @ Mediatek, this fixes packet reordering in MLO-STR
    mode, which caused TCP performance to suffer.  Patch is considered
    under development and may need further improvements.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/mediatek/mt76/mt7996/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/main.c
@@ -1708,8 +1708,12 @@ mt7996_mac_sta_deinit_link(struct mt7996_dev *dev, struct ieee80211_vif *vif,
 	if (!msta_link)
 		return;
 
-	for (i = 0; i < ARRAY_SIZE(msta_link->wcid.aggr); i++)
+	for (i = 0; i < ARRAY_SIZE(msta_link->wcid.aggr); i++) {
+		if (sta->mlo && !last_link)
+			rcu_assign_pointer(msta_link->wcid.aggr[i], NULL);
+		else
 			mt76_rx_aggr_stop(&dev->mt76, &msta_link->wcid, i);
+	}
 
 	if (!sta->mlo)
 		mt7996_mcu_add_sta(dev, vif, conf, link_sta, mconf, msta_link,
@@ -2183,10 +2187,13 @@ mt7996_ampdu_action(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	struct mt7996_sta *msta = (struct mt7996_sta *)sta->drv_priv;
 	struct ieee80211_txq *txq = sta->txq[params->tid];
 	struct ieee80211_link_sta *link_sta;
+	struct mt76_rx_tid *rx_tid = NULL;
 	u16 tid = params->tid;
 	u16 ssn = params->ssn;
 	struct mt76_txq *mtxq;
+	unsigned long valid_links = msta->valid_links ?: BIT(0);
 	unsigned int link_id;
+	int valid_link_cnt = hweight16((u16)valid_links);
 	int ret = 0;
 
 	if (!txq)
@@ -2200,6 +2207,7 @@ mt7996_ampdu_action(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 		struct mt7996_sta_link *msta_link;
 		struct mt7996_vif_link *link;
 
+		valid_link_cnt--;
 		msta_link = mt76_dereference(msta->link[link_id], &dev->mt76);
 		if (!msta_link)
 			continue;
@@ -2210,15 +2218,24 @@ mt7996_ampdu_action(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 
 		switch (action) {
 		case IEEE80211_AMPDU_RX_START:
-			mt76_rx_aggr_start(&dev->mt76, &msta_link->wcid, tid,
-					   ssn, params->buf_size);
+			if (!rx_tid) {
+				mt76_rx_aggr_start(&dev->mt76, &msta_link->wcid, tid,
+						   ssn, params->buf_size);
+				rx_tid = rcu_access_pointer(msta_link->wcid.aggr[tid]);
+			} else
+				rcu_assign_pointer(msta_link->wcid.aggr[tid], rx_tid);
+
 			ret = mt7996_mcu_add_rx_ba(dev, params, link,
 						   msta_link, true);
 			mtk_dbg(&dev->mt76, BA, "ampdu-action, RX_START, tid: %d ssn: %d ret: %d\n",
 				tid, ssn, ret);
 			break;
 		case IEEE80211_AMPDU_RX_STOP:
-			mt76_rx_aggr_stop(&dev->mt76, &msta_link->wcid, tid);
+			if (sta->mlo && valid_link_cnt > 0)
+				rcu_assign_pointer(msta_link->wcid.aggr[tid], NULL);
+			else
+				mt76_rx_aggr_stop(&dev->mt76, &msta_link->wcid, tid);
+
 			ret = mt7996_mcu_add_rx_ba(dev, params, link,
 						   msta_link, false);
 			mtk_dbg(&dev->mt76, BA, "ampdu-action, RX_STOP, tid: %d ssn: %d ret: %d\n",
