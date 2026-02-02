1fe4e3b55b8510e94eca6505c2cebb1668a3cc9a
Rory Little <rory@candelatech.com>
2026-02-02 13:23:14 -0800

    wifi: mt76: Reduce verbosity of queue debug logging.
    These get quite verbose, should only be on TXV path.
    
    Signed-off-by: Rory Little <rory@candelatech.com>
    CT-TODO: Squash into ce424145f461 ("wifi: mt76: Block tx if waiting too long to hear from any MSDUs.")

--- drivers/net/wireless/mediatek/mt76/tx.c
+++ drivers/net/wireless/mediatek/mt76/tx.c
@@ -1038,12 +1038,12 @@ mt76_token_release(struct mt76_dev *dev, int token, bool *wake)
 	if (tx_wait_thresh_ms && oldest_txwi &&
 	    time_is_before_jiffies(oldest_txwi->jiffies + (HZ * tx_wait_thresh_ms / 1000))) {
 		if (!dev->phy.q_tx[0]->blocked)
-			mt76_dbg(dev, MT76_DBG_WRN,
+			mt76_dbg(dev, MT76_DBG_TXV,
 				 "Tx queue blocked, clearing before allowing more transmits.");
 		__mt76_set_tx_blocked(dev, true);
 	} else if (dev->token_count < dev->token_size - MT76_TOKEN_FREE_THR &&
 		   dev->phy.q_tx[0]->blocked && !*wake) {
-		mt76_dbg(dev, MT76_DBG_WRN, "Restarting previously blocked queue.");
+		mt76_dbg(dev, MT76_DBG_TXV, "Restarting previously blocked queue.");
 		*wake = true;
 	}
 
