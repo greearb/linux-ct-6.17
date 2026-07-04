greearb/linux-ct-5.15

mac80211/mtk: More thorough OFDMA disabling.
Still does not work around MTK station download problem though.

Signed-off-by: Ben Greear <greearb@candelatech.com>

net/mac80211/util.c
@@ -3107,8 +3107,14 @@ u8 *ieee80211_ie_build_he_cap(struct ieee80211_sub_if_data *sdata, u8 *pos,
 		hec = (struct ieee80211_he_cap_elem *)(pos);
 		hec->mac_cap_info[3] &= ~IEEE80211_HE_MAC_CAP3_OFDMA_RA;
 		hec->mac_cap_info[5] &= ~IEEE80211_HE_MAC_CAP5_HT_VHT_TRIG_FRAME_RX;
+		hec->phy_cap_info[3] &= ~IEEE80211_HE_PHY_CAP3_RX_PARTIAL_BW_SU_IN_20MHZ_MU;
 		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_SU_BEAMFORMING_FB;
 		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_MU_BEAMFORMING_PARTIAL_BW_FB;
+		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_CQI_FB;
+
+		/* Disable MU related OFDMA stuff too */
+		hec->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_PARTIAL_BANDWIDTH_DL_MUMIMO;
+		hec->phy_cap_info[2] &= ~IEEE80211_HE_PHY_CAP2_UL_MU_PARTIAL_MU_MIMO;
 	}
 
 	pos += sizeof(he_cap->he_cap_elem);

greearb/linux-ct-6.17

net/mac80211/util.c
@@ -2584,8 +2584,14 @@ ieee80211_get_adjusted_he_cap(const struct ieee80211_conn_settings *conn,
 		pr_info("adjust-he-cap, disabling OFDMA.");
 		elem->mac_cap_info[3] &= ~IEEE80211_HE_MAC_CAP3_OFDMA_RA;
 		elem->mac_cap_info[5] &= ~IEEE80211_HE_MAC_CAP5_HT_VHT_TRIG_FRAME_RX;
+		elem->phy_cap_info[3] &= ~IEEE80211_HE_PHY_CAP3_RX_PARTIAL_BW_SU_IN_20MHZ_MU;
 		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_SU_BEAMFORMING_FB;
 		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_MU_BEAMFORMING_PARTIAL_BW_FB;
+		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_TRIG_CQI_FB;
+
+		/* Disable MU related OFDMA stuff too */
+		elem->phy_cap_info[6] &= ~IEEE80211_HE_PHY_CAP6_PARTIAL_BANDWIDTH_DL_MUMIMO;
+		elem->phy_cap_info[2] &= ~IEEE80211_HE_PHY_CAP2_UL_MU_PARTIAL_MU_MIMO;
 	}
 }
 